159a05d4fd702ed4e56d0cc00a925cce45387a12
OugMontiel <88790286+OugMontiel@users.noreply.github.com>
2024-04-22 17:43:45 -0500

    feat: :sparkles: ejercicio 3
    comillas invertida y $ mas {} concatena ... similar a f en python

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import { getAllOficceAndCodeCity, getAllOfficceCityAndMovil } from "./modulos/offices.js";
+import { getAllFullNameAndEmails } from "./modulos/employee.js";
 
-console.log(await getAllOfficceCityAndMovil())
+console.log(await getAllFullNameAndEmails())
---
+++ js/modulos/employee.js
@@ -0,0 +1,13 @@
+// 3. Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
+export const getAllFullNameAndEmails=async()=>{
+    let res = await fetch("http://localhost:5103/employee?code_boss=7")
+    let data = await res.json();
+    let dataUpdate = data.map(val=>{
+        return{
+            name:val.name,
+            lastName: `${val.lastname1} ${val.lastname2}`,
+            email:val.email
+        }
+    })
+    return dataUpdate
+}
